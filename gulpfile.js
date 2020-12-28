e2f75d21352cb72e8ab5172e99731faee0908a88
Sergei <example@gmail.com>
2020-12-28 22:52:18 +0300

    Carousel complete

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('server', function() {
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(rename({suffix: '', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
