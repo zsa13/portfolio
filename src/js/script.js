a248016cf6365a2e13e6f79ac0aa9a8e76aa9865
Sergey <zavyalov.sergey.a@gmail.com>
2021-01-12 21:34:50 +0300

    Add modal and mobile adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -33,4 +33,46 @@ $(document).ready(function(){
 
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
+
+  // Modal
+
+  $('[data-modal=consultation]').on('click', function(){
+      $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+  });
+
+  $('.button_mini').each(function(i){
+      $(this).on('click', function(){
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('slow');
+      });
+  });
+
+  // Valid
+
+  function valideForms(form) {
+    $(form).validate({
+        rules: {
+            name: "required",
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }  
+        },
+        messages: {
+          name: "Пожалуйста, введите свое имя",
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправильно введен адрес почты"
+          }
+        }
+    });
+  };
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
 });
